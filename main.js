97425b5493f4427fc529351008b065cf0d4aa141
Norbert B. Menyhart <72194361+NorbertBM@users.noreply.github.com>
2022-06-18 07:06:19 +0200

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-console.log("log");
-
-const allNavTags = document.querySelectorAll("#mainNav a");
-
-allNavTags.forEach(() => (navTag.onclick = () => console.log(navTag)));
-
-function toggleActive(element) {
-  element.className.toggle("active");
-}
